2a08e2c7dd72adb8778745e3118b641aeb32aa29
RosaliaNjoki <njosali0@gmail.com>
2024-03-05 14:10:49 +0300

    articles displaed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-
-
 function Article ({title, date="January 1, 1970", preview}){
+    
     return(
         <article>
             <h3>{title}</h3>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Article from "./Article";
 
-function ArticleList(){
-    return(
+const ArticleList=({posts})=>{
+      return(
         <main>
-            <Article />;
+            {posts.map((post, id)=>(
+                <Article key={id} title={post.title} date={post.date} preview={post.preview} />
+
+            ))}
+            
         </main>
     );
+
 }
 export default ArticleList;
